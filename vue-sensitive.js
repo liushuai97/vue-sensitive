6e979b26aa63aa2442420fd014284a6a207222f3
Anily <liushuai1993@outlook.com>
2022-04-21 11:10:31 +0800

    更新注册方式

--- vue-sensitive.js
+++ vue-sensitive.js
@@ -1,9 +1,8 @@
-import _Vue from 'vue';
 import sensitive from './src/lib/sensitive';
 
 const Sensitive = (Vue) => {
   if (typeof window !== 'undefined' && window.Vue) {
-    Vue = window.Vue || _Vue;
+    Vue = window.Vue;
   }
   Vue.component(sensitive.name, sensitive.install);
 };
